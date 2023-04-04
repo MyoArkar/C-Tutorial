b22def48840e2c00a7cd41a975a42de4a305c93e
MyoArkar <myoarkar430@gmail.com>
2023-04-04 16:14:59 +0630

    tuto9

---
+++ Tutorial9/Tutorial9/ListForm.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Tutorial9
+{
+    partial class ListForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txt_search = new System.Windows.Forms.TextBox();
+            this.btn_search = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.useridDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.usernameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.passwordDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.genderDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.addressDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.muserBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.user_management_system_dbDataSet = new Tutorial9.user_management_system_dbDataSet();
+            this.m_userTableAdapter = new Tutorial9.user_management_system_dbDataSetTableAdapters.m_userTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.muserBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.user_management_system_dbDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_search
+            // 
+            this.txt_search.Location = new System.Drawing.Point(171, 44);
+            this.txt_search.Name = "txt_search";
+            this.txt_search.Size = new System.Drawing.Size(231, 27);
+            this.txt_search.TabIndex = 0;
+            this.txt_search.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btn_search
+            // 
+            this.btn_search.Location = new System.Drawing.Point(447, 36);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(154, 52);
+            this.btn_search.TabIndex = 1;
+            this.btn_search.Text = "Search";
+            this.btn_search.UseVisualStyleBackColor = true;
+            this.btn_search.Click += new System.EventHandler(this.btn_search_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.useridDataGridViewTextBoxColumn,
+            this.usernameDataGridViewTextBoxColumn,
+            this.passwordDataGridViewTextBoxColumn,
+            this.genderDataGridViewTextBoxColumn,
+            this.addressDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.muserBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(39, 112);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 66;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(880, 497);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // useridDataGridViewTextBoxColumn
+            // 
+            this.useridDataGridViewTextBoxColumn.DataPropertyName = "user_id";
+            this.useridDataGridViewTextBoxColumn.HeaderText = "user_id";
+            this.useridDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.useridDataGridViewTextBoxColumn.Name = "useridDataGridViewTextBoxColumn";
+            this.useridDataGridViewTextBoxColumn.ReadOnly = true;
+            this.useridDataGridViewTextBoxColumn.Width = 160;
+            // 
+            // usernameDataGridViewTextBoxColumn
+            // 
+            this.usernameDataGridViewTextBoxColumn.DataPropertyName = "user_name";
+            this.usernameDataGridViewTextBoxColumn.HeaderText = "user_name";
+            this.usernameDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.usernameDataGridViewTextBoxColumn.Name = "usernameDataGridViewTextBoxColumn";
+            this.usernameDataGridViewTextBoxColumn.ReadOnly = true;
+            this.usernameDataGridViewTextBoxColumn.Width = 160;
+            // 
+            // passwordDataGridViewTextBoxColumn
+            // 
+            this.passwordDataGridViewTextBoxColumn.DataPropertyName = "password";
+            this.passwordDataGridViewTextBoxColumn.HeaderText = "password";
+            this.passwordDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.passwordDataGridViewTextBoxColumn.Name = "passwordDataGridViewTextBoxColumn";
+            this.passwordDataGridViewTextBoxColumn.ReadOnly = true;
+            this.passwordDataGridViewTextBoxColumn.Width = 160;
+            // 
+            // genderDataGridViewTextBoxColumn
+            // 
+            this.genderDataGridViewTextBoxColumn.DataPropertyName = "gender";
+            this.genderDataGridViewTextBoxColumn.HeaderText = "gender";
+            this.genderDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.genderDataGridViewTextBoxColumn.Name = "genderDataGridViewTextBoxColumn";
+            this.genderDataGridViewTextBoxColumn.ReadOnly = true;
+            this.genderDataGridViewTextBoxColumn.Width = 160;
+            // 
+            // addressDataGridViewTextBoxColumn
+            // 
+            this.addressDataGridViewTextBoxColumn.DataPropertyName = "address";
+            this.addressDataGridViewTextBoxColumn.HeaderText = "address";
+            this.addressDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.addressDataGridViewTextBoxColumn.Name = "addressDataGridViewTextBoxColumn";
+            this.addressDataGridViewTextBoxColumn.ReadOnly = true;
+            this.addressDataGridViewTextBoxColumn.Width = 160;
+            // 
+            // muserBindingSource
+            // 
+            this.muserBindingSource.DataMember = "m_user";
+            this.muserBindingSource.DataSource = this.user_management_system_dbDataSet;
+            // 
+            // user_management_system_dbDataSet
+            // 
+            this.user_management_system_dbDataSet.DataSetName = "user_management_system_dbDataSet";
+            this.user_management_system_dbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // m_userTableAdapter
+            // 
+            this.m_userTableAdapter.ClearBeforeFill = true;
+            // 
+            // ListForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1323, 621);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_search);
+            this.Controls.Add(this.txt_search);
+            this.Name = "ListForm";
+            this.Text = "ListForm";
+            this.Load += new System.EventHandler(this.ListForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.muserBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.user_management_system_dbDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_search;
+        private System.Windows.Forms.Button btn_search;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private user_management_system_dbDataSet user_management_system_dbDataSet;
+        private System.Windows.Forms.BindingSource muserBindingSource;
+        private user_management_system_dbDataSetTableAdapters.m_userTableAdapter m_userTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn useridDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn usernameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn passwordDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn genderDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn addressDataGridViewTextBoxColumn;
+    }
+}
---
+++ Tutorial9/Tutorial9/ListForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tutorial9
+{
+    public partial class ListForm : Form
+    {
+        public ListForm()
+        {
+            InitializeComponent();
+        }
+        public static SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-GAIQ5AI;Initial Catalog=user_management_system_db;Integrated Security=True");
+        SqlDataAdapter adapt;
+        DataTable dt;
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ListForm_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'user_management_system_dbDataSet.m_user' table. You can move, or remove it, as needed.
+            this.m_userTableAdapter.Fill(this.user_management_system_dbDataSet.m_user);
+            conn.Open();
+            adapt = new SqlDataAdapter("select * from m_user", conn);
+            dt = new DataTable();
+            adapt.Fill(dt);
+            dataGridView1.DataSource = dt;
+            conn.Close();
+
+        }
+
+        private void btn_search_Click(object sender, EventArgs e)
+        {
+            
+            conn.Open();
+            SqlCommand cmd= new SqlCommand("select * from m_user where user_id like @user_id or user_name like @user_name or address like @address", conn);
+            cmd.Parameters.AddWithValue("@user_id", string.Format("%{0}%", txt_search.Text));
+            cmd.Parameters.AddWithValue("@user_Name", string.Format("%{0}%", txt_search.Text));
+            cmd.Parameters.AddWithValue("@address", string.Format("%{0}%", txt_search.Text));
+            adapt = new SqlDataAdapter(cmd);
+            dt = new DataTable();
+            adapt.Fill(dt);
+            if(dt.Rows.Count > 0)
+            {
+                dataGridView1.DataSource = dt;
+                conn.Close();
+                MessageBox.Show("data exist");
+            }
+            else
+            {
+                conn.Close();
+                MessageBox.Show("data not found");
+            }
+        }
+    }
+}
